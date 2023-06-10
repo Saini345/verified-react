7715ae2fc2750ae20ec0c3efe0e490b4f26c7873
saini345 <sainipoojabagdi@gmail.com>
2023-06-10 21:55:43 +0530

    first commit

---
+++ src/Component/Footer.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import "./Footer.css"
+function Footer() {
+  return (
+    <div>
+      <div>
+        <div className="container margin_top_100">
+          <table className="table">
+            <thead className=''>
+              <tr>
+                <th className="data"><a><img className="Group-17326-img" src="images/Group 17326.png" /></a></th>
+                <th className="data table-th">Individuals</th>
+                <th className="data table-th">Businesses</th>
+                <th className="data table-th">Service Providers</th>
+                <th className="data table-th">Company</th>
+                <th className="data table-th">Partners</th>
+              </tr>
+            </thead>
+            <tbody className="table-td">
+              <tr>
+                <td className="data"></td>
+                <td className="data">Account</td>
+                <td className="data">Account</td>
+                <td className="data">Asset Origination</td>
+                <td className="data">About us</td>
+                <td className="data">Operators</td>
+              </tr>
+              <tr>
+                <td className="data"></td>
+                <td className="data">Assets</td>
+                <td className="data">Assets</td>
+                <td className="data">Asset Management</td>
+                <td className="data">Assets</td>
+                <td className="data">Developers</td>
+              </tr>
+              <tr>
+                <td className="data"></td>
+                <td className="data">Deposits</td>
+                <td className="data">Deposits</td>
+                <td className="data">Asset Servicing</td>
+                <td className="data">Careers</td>
+                <td className="data">Investors</td>
+              </tr>
+              <tr>
+                <td className="data"></td>
+                <td className="data">Payment</td>
+                <td className="data">Payment</td>
+                <td className="data">Payments</td>
+                <td className="data">Contact Us</td>
+
+              </tr>
+              <tr>
+                <td className="data"></td>
+                <td className="data">Investment</td>
+                <td className="data">Finance</td>
+                <td className="data">Network Operators</td>
+                <td className="data">Blog</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
